f8ce009c18c898f1f12c271dd08ffa65af29244c
mik9016 <mikolajgruszecki@o2.pl>
2021-01-20 07:15:39 +0100

    paradigm change

--- src/app.js
+++ src/app.js
@@ -1,93 +1,138 @@
-import style from "./css/index.scss"
+import style from "./css/index.scss";
 
 const minutes = document.getElementById('min');
 const seconds =document.getElementById('sec');
 const btn = document.querySelector('button');
-
-minutes.defaultValue = 0;
-seconds.defaultValue = 0;
-
-let currentSecValue = seconds.value;
-let updatedSecValue;
-let currentMinValue = minutes.value;
-let updatedMinValue;
+const inputs = document.querySelectorAll('input');
 
 
+// minutes.defaultValue = 1;
+// seconds.defaultValue = 3;
 
-function counter(){
-    // seconds.addEventListener('change', e => {
-    //     if(currentMinValue>minutes.getAttribute('min')){
-                
-    //         updatedMinValue = parseInt(currentMinValue)-1;
-    //         minutes.setAttribute('value', parseInt(currentMinValue)-1);
-    //         currentMinValue = updatedMinValue;
-    //         updatedSecValue = 59; 
-    // })
+// let currentSecValue = seconds.value;
+// let updatedSecValue;
+// let currentMinValue = minutes.value;
+// let updatedMinValue;
 
-    if( seconds.value>0){
+// function minUp(){
+//     minutes.addEventListener('change', e =>{
+//         updatedMinValue = e.target.value;
+//         currentMinValue = updatedMinValue;
+//        console.log(currentMinValue);
+//        secUpdate();
+//        minUpdate(e);
+//     });
+
+// }
+
+// function secUpdate() {
+
+//     seconds.addEventListener('change', e =>{
+   
+//     if(e.target.value === seconds.getAttribute('max')){
+        
+        
+//         updatedMinValue = parseInt(currentMinValue)+1;
+//         minutes.setAttribute('value', parseInt(currentMinValue)+1);
+//         currentMinValue = updatedMinValue;
+//         e.target.value = 0;   
+//     } else if(e.target.value === seconds.getAttribute('min')){
+        
+//         console.log(currentMinValue + " seconds listener")
+//         updatedMinValue = parseInt(currentMinValue)-1;
+//         minutes.setAttribute('value', parseInt(currentMinValue)-1);
+//         currentMinValue = updatedMinValue;
+//         e.target.value = 59; 
+//     }
+//      console.log(currentMinValue + ' inside secUpdate')
+// });
+
+// };
+
+// function minUpdate(e){
+//         if(e.target.value === seconds.getAttribute('min')){
+//             console.log(currentMinValue + " inside minUpdate")
+//             updatedMinValue = parseInt(currentMinValue)-1;
+//             minutes.setAttribute('value', parseInt(currentMinValue)-1);
+//             currentMinValue = updatedMinValue;
+//             e.target.value = 59;   
+//         }
+//         // updatedMinValue = e.target.value;
+//         // currentMinValue = updatedMinValue;
        
-        currentSecValue = seconds.value;
-        updatedSecValue = currentSecValue -1;
-        seconds.disabled = true;
+        
+       
+// }
 
-        currentSecValue = updatedSecValue;
-        seconds.value = currentSecValue;
+// function counter(){
+        
+//     if( seconds.value>0){
+       
+//         currentSecValue = seconds.value;
+//         updatedSecValue = currentSecValue -1;
+//         seconds.disabled = true;
+
+//         currentSecValue = updatedSecValue;
+//         seconds.value = currentSecValue;
         
         
 
 
 
-       setTimeout(counter,1000)
+//        setTimeout(counter,1000)
+//     }
+//     else {
+//         alert('The End!')
+//         seconds.disabled = false;
+//     }
+// };
+
+// minUp();
+
+
+// btn.addEventListener('click',event=> {
+//     event.preventDefault();
+//     counter();
+
+// });
+
+
+let state = {
+    currentMinValue: '',
+    currentSecValue: '',
+    updatedMinValue: '',
+    updatedSecValue: ''
+}
+
+state.currentMinValue = 0;
+state.currentSecValue = 0;
+minutes.value = 0;
+seconds.value = 0;
+
+inputs.forEach(input => {input.addEventListener('change', e => {
+    console.log(e.target.value)
+
+    if(e.target.id === 'sec'){
+        state.currentSecValue = +e.target.value;
+        if (state.currentSecValue === 60) {
+            state.currentMinValue = +state.currentMinValue +1;
+            console.log(state.currentMinValue + ' minvalue')
+            minutes.value = state.currentMinValue;
+            seconds.value = 0;
+        }else if(state.currentMinValue>0 && state.currentSecValue === 0){
+            state.currentMinValue = +state.currentMinValue -1;
+            minutes.value = state.currentMinValue;
+            seconds.value = 59;
+        }
+    }else if(e.target.id === 'min'){
+        state.currentMinValue = +e.target.value;
     }
-    else {
-        alert('The End!')
-        seconds.disabled = false;
-    }
-};
-
-
-
-   seconds.addEventListener('change', e => {
-
-
-       if(e.target.value === seconds.getAttribute('max')){
     
-        
-        updatedMinValue = parseInt(currentMinValue)+1
-        minutes.setAttribute('value', parseInt(currentMinValue)+1);
-        currentMinValue = updatedMinValue;
-        e.target.value = 0;   
-    }
-    
-        // if(e.target.value === seconds.getAttribute('min')){
-
-        //     if(currentMinValue>minutes.getAttribute('min')){
-                
-        //         updatedMinValue = parseInt(currentMinValue)-1;
-        //         minutes.setAttribute('value', parseInt(currentMinValue)-1);
-        //         currentMinValue = updatedMinValue;
-        //         e.target.value = 59;
-             
-        //     }
-        // }
-        
-        
-        
-    
-   });
-
-   minutes.addEventListener('change', e => {
-        updatedMinValue = e.target.value;
-        currentMinValue = updatedMinValue;
-
-   });
-
-    
+})});
 
 
 
-btn.addEventListener('click',event=> {
-    event.preventDefault();
-    counter();
 
-});
+
+
+
