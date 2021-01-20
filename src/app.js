2afe9f42466e2aa848c98dbf0fbd4b40a9108cd0
mik9016 <mikolajgruszecki@o2.pl>
2021-01-20 12:19:36 +0100

    funcionality working - adding counter funcion

--- src/app.js
+++ src/app.js
@@ -6,102 +6,10 @@ const btn = document.querySelector('button');
 const inputs = document.querySelectorAll('input');
 
 
-// minutes.defaultValue = 1;
-// seconds.defaultValue = 3;
-
-// let currentSecValue = seconds.value;
-// let updatedSecValue;
-// let currentMinValue = minutes.value;
-// let updatedMinValue;
-
-// function minUp(){
-//     minutes.addEventListener('change', e =>{
-//         updatedMinValue = e.target.value;
-//         currentMinValue = updatedMinValue;
-//        console.log(currentMinValue);
-//        secUpdate();
-//        minUpdate(e);
-//     });
-
-// }
-
-// function secUpdate() {
-
-//     seconds.addEventListener('change', e =>{
-   
-//     if(e.target.value === seconds.getAttribute('max')){
-        
-        
-//         updatedMinValue = parseInt(currentMinValue)+1;
-//         minutes.setAttribute('value', parseInt(currentMinValue)+1);
-//         currentMinValue = updatedMinValue;
-//         e.target.value = 0;   
-//     } else if(e.target.value === seconds.getAttribute('min')){
-        
-//         console.log(currentMinValue + " seconds listener")
-//         updatedMinValue = parseInt(currentMinValue)-1;
-//         minutes.setAttribute('value', parseInt(currentMinValue)-1);
-//         currentMinValue = updatedMinValue;
-//         e.target.value = 59; 
-//     }
-//      console.log(currentMinValue + ' inside secUpdate')
-// });
-
-// };
-
-// function minUpdate(e){
-//         if(e.target.value === seconds.getAttribute('min')){
-//             console.log(currentMinValue + " inside minUpdate")
-//             updatedMinValue = parseInt(currentMinValue)-1;
-//             minutes.setAttribute('value', parseInt(currentMinValue)-1);
-//             currentMinValue = updatedMinValue;
-//             e.target.value = 59;   
-//         }
-//         // updatedMinValue = e.target.value;
-//         // currentMinValue = updatedMinValue;
-       
-        
-       
-// }
-
-// function counter(){
-        
-//     if( seconds.value>0){
-       
-//         currentSecValue = seconds.value;
-//         updatedSecValue = currentSecValue -1;
-//         seconds.disabled = true;
-
-//         currentSecValue = updatedSecValue;
-//         seconds.value = currentSecValue;
-        
-        
-
-
-
-//        setTimeout(counter,1000)
-//     }
-//     else {
-//         alert('The End!')
-//         seconds.disabled = false;
-//     }
-// };
-
-// minUp();
-
-
-// btn.addEventListener('click',event=> {
-//     event.preventDefault();
-//     counter();
-
-// });
-
 
 let state = {
     currentMinValue: '',
-    currentSecValue: '',
-    updatedMinValue: '',
-    updatedSecValue: ''
+    currentSecValue: ''
 }
 
 state.currentMinValue = 0;
@@ -110,7 +18,7 @@ minutes.value = 0;
 seconds.value = 0;
 
 inputs.forEach(input => {input.addEventListener('change', e => {
-    console.log(e.target.value)
+    
 
     if(e.target.id === 'sec'){
         state.currentSecValue = +e.target.value;
@@ -131,6 +39,38 @@ inputs.forEach(input => {input.addEventListener('change', e => {
 })});
 
 
+
+function counter(){
+        
+    if( state.currentSecValue>0){
+  
+        state.currentSecValue = +state.currentSecValue -1;
+        seconds.value = state.currentSecValue;
+        seconds.disabled = true;
+        minutes.disabled = true;
+        if(state.currentMinValue>0 && state.currentSecValue === 0){
+            
+            state.currentMinValue = +state.currentMinValue - 1;
+            minutes.value = state.currentMinValue;
+            seconds.value = state.currentSecValue = 59;
+            
+        }
+        
+       setTimeout(counter,1000)
+    }
+    else {
+        alert('The End!')
+        seconds.disabled = false;
+        minutes.disabled = false;
+    }
+};
+
+
+btn.addEventListener('click',event=> {
+    event.preventDefault();
+    counter();
+
+});
 
 
 
